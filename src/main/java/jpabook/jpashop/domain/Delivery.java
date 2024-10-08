6657d5cb1e3830363d691e94d1aeb642de61a090
HoChoRoo <rttkhc0304@gmail.com>
2024-10-08 20:20:36 +0900

    Entity 연관관계 매핑

--- src/main/java/jpabook/jpashop/domain/Delivery.java
+++ src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -1,4 +1,26 @@
 package jpabook.jpashop.domain;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
 public class Delivery {
+
+    @Id @GeneratedValue
+    @Column(name = "delivery_id")
+    private Long id;
+
+    @OneToOne(mappedBy = "delivery")
+    private Order order;
+
+    @Embedded
+    private Address address;
+                                    //ORDINAL, STRING이 있는데 STRING 으로 쓰자
+    @Enumerated(EnumType.STRING)   // ORDINAL 은 1, 2, 3 순서로 들어가는데 중간에 추가되면 복잡함
+    private DeliveryStatus status ;//READY, COMP
+
+
 }
