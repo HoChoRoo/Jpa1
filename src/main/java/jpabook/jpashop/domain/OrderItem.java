431caf7c6ff0f149678738d51f2b6b88adbde925
HoChoRoo <rttkhc0304@gmail.com>
2024-10-15 20:05:44 +0900

    Order 도메인 개발

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -2,12 +2,16 @@ package jpabook.jpashop.domain;
 
 import jakarta.persistence.*;
 import jpabook.jpashop.domain.item.Item;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class OrderItem {
+
 
     @Id @GeneratedValue
     @Column(name = "order_item_id")
@@ -25,4 +29,24 @@ public class OrderItem {
 
     private int count;
 
+    //==생성 메서드 ==
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count){
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count);
+        return orderItem;
+    }
+
+    // ==비지니스 로직 ==
+    public void cancel() {
+        getItem().addStock(count);
+    }
+
+    // == 조회 로직 ==
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
 }
